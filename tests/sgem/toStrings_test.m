b6f4a1048a3b46a00c69d616675ebc3509260671
Unknown <jdbancal@gmail.com>
2019-05-24 20:54:02 +0200

    what is happening on travis?

--- tests/sgem/toStrings_test.m
+++ tests/sgem/toStrings_test.m
@@ -8,6 +8,8 @@ end
 
 function test_consistency
     text = {'3.141592653589793238462643383279502884197169399375101', '2.718281828'};
+    disp(toStrings(sgem('pi')));
+    disp(toStrings(sgem('e'),10));
     assert(isequal(toStrings(sgem('pi')), text{1}));
     assert(isequal(toStrings(sgem('e'),10), text{2}));
 end

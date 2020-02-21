cbea6c3ba5b0cbacfe3e6c1af36981b2598f87b2
Jean-Daniel Bancal <jdbancal@gmail.com>
2020-02-21 21:39:32 +0100

    matlab doesn't have a 'clean' function

--- tests/sgem/clean_test.m
+++ tests/sgem/clean_test.m
@@ -9,21 +9,14 @@ end
 function test_consistency
     x = generateMatrices(2, 5, {'A', 'AR', 'AI'});
 
-    % Octave doesn't have the clean function
-    isOctave = exist('OCTAVE_VERSION', 'builtin') ~= 0;
-    if isOctave
-        for i = 1:length(x)
-            y = x{i}/250;
-            yr = real(y);
-            yi = imag(y);
-            yr(find(abs(yr) < 0.01)) = 0;
-            yi(find(abs(yi) < 0.01)) = 0;
-            y = yr + yi*1i;
-            assert(max(max(abs( clean(x{i}/250, 0.01) - y ))) < 1e-6);
-        end
-    else
-        validateDoubleConsistency(@(x) clean(x/250, 0.01), x);
-        validateDoubleConsistency(@(x) clean(double(x)/250, sgem(0.01)), x);
+    for i = 1:length(x)
+        y = x{i}/250;
+        yr = real(y);
+        yi = imag(y);
+        yr(find(abs(yr) < 0.01)) = 0;
+        yi(find(abs(yi) < 0.01)) = 0;
+        y = yr + yi*1i;
+        assert(max(max(abs( clean(x{i}/250, 0.01) - y ))) < 1e-6);
     end
     
     previousPrecision = gem.workingPrecision;

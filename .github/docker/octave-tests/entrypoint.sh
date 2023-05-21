cbec87b60d3eac4e9de9144c6a7b3c39c797fca4
Jean-Daniel Bancal <jdbancal.physics@gmail.com>
2023-05-21 22:44:47 +0200

    initial attempt auto build script

--- .github/docker/octave-tests/entrypoint.sh
+++ .github/docker/octave-tests/entrypoint.sh
@@ -6,31 +6,39 @@ echo "Executing entrypoint.sh"
 
 echo argument=$1
 
+# display current folder
 pwd
 ls -al
 
 git rev-parse HEAD
 
-export ADDPATH_COMMAND="replab_init('verbose', 2);"
-export COVERING=false
+export ADDPATH_COMMAND=""
 
-TEST_COMMAND="exit(~replab_runtests('slowtests', false, 'notebooktests', false, 'withCoverage', $COVERING));";
-echo "TEST_COMMAND=$TEST_COMMAND";
+export BUILD_BINARIES="make;";
+echo "BUILD_BINARIES| $BUILD_BINARIES";
+
+export COVERING=true
+export TEST_COMMAND="exit(~run_tests($COVERING,1));";
+echo "TEST_COMMAND| $TEST_COMMAND";
+
+# Check GCC version
+gcc --version
 
 # Check what octave packages we have installed
 octave -q --eval "ver"
 
-# Check that octave can access java
+# Check if octave can access java
 octave --eval "b = javaMethod('valueOf', 'java.math.BigInteger', 2)"
 
 # Remove any cached results files from previous build, if present
 rm -f testresults.xml;
 
 # Run tests
-if octave -q --eval "$ADDPATH_COMMAND $TEST_COMMAND"; then
+if octave -q --eval "$ADDPATH_COMMAND $BUILD_BINARIES $TEST_COMMAND"; then
   # Check where we ended up and what's going on where we are
   pwd
   ls -alh
+  ls -alh gem
   if [ $COVERING == true ]; then
     bash <(curl -s https://codecov.io/bash);
   fi
